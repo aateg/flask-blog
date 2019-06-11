e441a11b818a26c6a66b77aaefad10a7d90afae1
Guilherme Fernandes <aateg@live.com>
2019-06-11 13:03:39 -0300

    Form, Forms Validation, register feedback

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+# protect against modify cookies and attacks
+app.config['SECRET_KEY'] = '3c72ee2b775aff54d0746b9e604c988d'
 
 posts = [
     {
@@ -20,9 +24,24 @@ posts = [
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    login = LoginForm()
+    return render_template('login.html', title='Login', login=login)
+
 
 if(__name__ == '__main__'):
     app.run(debug = True)
