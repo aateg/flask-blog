05c1704df4647d43df47d18548229992975f66c7
Guilherme Fernandes <aateg@live.com>
2019-06-21 14:04:28 -0300

    authentication methods completed

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy  import SQLAlchemy
+from flask_bcrypt import Bcrypt # para encryptar a senha
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,5 +9,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '3c72ee2b775aff54d0746b9e604c988d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) # database
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # verifica se está logado para acessar a rota account
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
