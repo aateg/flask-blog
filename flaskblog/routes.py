1468448de037ad51742311aadb2e048f59822a93
Guilherme Fernandes <aateg@live.com>
2019-06-26 10:33:19 -0300

    Now it's able to add posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,29 +3,14 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-
-posts = [
-    {
-        'author': 'Gonçalves Silva',
-        'title': 'Blog Post 1',
-        'content': 'Post Content 1',
-        'date_posted': 'June 10, 2019'
-    },
-    {
-        'author': 'Fernandes Gonçalves',
-        'title': 'Blog Post 2',
-        'content': 'Post Content 2',
-        'date_posted': 'June 10, 2019'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all() # before was dummy data - a list of dictionaries
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -91,10 +76,22 @@ def account():
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
-        flash('Your account has been updated!','sucess')
+        flash('Your account has been updated!','success')
         return redirect(url_for('account'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static', filename=f'profile_pics/{current_user.image_file}')
     return render_template('account.html', title='account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
