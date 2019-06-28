fde8404fa88465f0e44e26ec130eab80c4ba41d9
Guilherme Fernandes <aateg@live.com>
2019-06-28 10:40:46 -0300

    sortering posts and filtering by authors

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -12,7 +12,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 def home():
     #posts = Post.query.all() # before was dummy data - a list of dictionaries
     page = request.args.get('page', 1, type=int) # get(key, default=None, type=None)
-    posts = Post.query.paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -130,3 +130,13 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    #posts = Post.query.all() # before was dummy data - a list of dictionaries
+    page = request.args.get('page', 1, type=int) # get(key, default=None, type=None)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
